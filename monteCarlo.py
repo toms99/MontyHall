63fab3e1fac170caabb2a230b2180b25479c568f
Tomás Segura <38998436+toms99@users.noreply.github.com>
2020-11-30 02:15:55 -0800

    Update monteCarlo.py

--- monteCarlo.py
+++ monteCarlo.py
@@ -1,121 +1,160 @@
 import random
 
-def init():
-    '''puertasTotales = int(input("Inserta el total de puertas: "))
-    puertasPorAbrir = int(input("Inserta el número de puertas por abrir: "))
-    cambiar = bool(input("¿Desea que el usuario cambie la puerta? "))
-    casos = int(input("¿Cuántos casos para Monte Carlo?: "))'''
-    
-    puertasTotales = 3
-    puertasPorAbrir = 1
-    cambiar = 1
-    casos = 10
-    MonteCarlo(casos, puertasTotales, puertasPorAbrir, cambiar)
-
-
-def MonteCarlo(casos, puertasTotales, puertasPorAbrir, cambiar):
-    datos = []
-    while casos > 0:
-        puertaUsuario = random.randrange(puertasTotales)
-        puertas = definirPuertas(puertasTotales, puertaUsuario)
-        result = abrirPuertas(puertasTotales, puertasPorAbrir, puertas, cambiar)
-        datos.append(result)
-        casos -= 1
-    print("El censo: ", datos)
-    print(Average(datos))
-
-def Average(lst): 
-    return sum(lst) / len(lst) 
-
-def definirPuertas(numeroTotal, puertaUsuario):
+''' Crea una lista con puertas cerradas segun el total
+param: numeroTotal -> total de puertas requerido
+retorna la lista de puertas cerradas''' 
+def definirPuertas(numeroTotal):
     # Definimos la lista de puertas y la posicion en
     # la que colocaremos el premio.
     puertas = []
-    premio = random.randrange(numeroTotal)
     
     # Insertamos el total de puertas con valor neutro.
     while (numeroTotal > 0):
         puertas.append(0)
         numeroTotal-=1
-    puertas[premio] = 1
-    
-    # Si el premio se ubica en la misma puerta que desea abrir el 
-    # usuario entonces lo identificamos con -3.
-    if (premio == puertaUsuario -1):
-        puertas[premio] = -3
-    else:
-    # Si no se ubican en la misma puerta entonces identificamos el
-    # premio con 1 y la puerta del usuario con 3.
-        puertas[premio] = 1
-        puertas[puertaUsuario -1] = 3
         
     # Retornamos el estado de todas las puertas.
     return puertas
-    
 
-def abrirPuertas(puertasTotales, puertasPorAbrir, puertas, cambiar):
-    # Definimos una lista de puertas abiertas.
-    abiertas = []
+''' Abre las puertas conviertiendolas en 1.
+param: puertasPorAbrir -> numero de puertas que se desean abrir.
+param: puertas -> lista de puertas cerradas.
+param: usuario -> puerta que escogio el usuario
+param: premio -> numero de puerta en el que se encuentra el premio
+Retorna una lista con dos listas: la lista de puertas abiertas y cerradas y la 
+lista de puertas abiertas. '''
+def abrirPuertas(puertasPorAbrir, puertas, usuario, premio):
+    return abrirPuertasAux(puertasPorAbrir, puertas,usuario, premio,  [])
+
+''' Funcion auxiliar para abrirPuertas() ya que es recursiva.'''
+def abrirPuertasAux(puertasPorAbrir, puertas, usuario, premio, abiertas):
+    puertasTotales = len(puertas)
     
-    # Comenzamos a abrir las puertas utilizando posiciones random.
-    while puertasPorAbrir > 0:
-        # Escogemos una posicion random para abrir la puerta.
-        ranInt = random.randrange(puertasTotales)
+    # Escogemos una posicion random para abrir la puerta.
+    ranInt = random.randrange(puertasTotales)
+    
+    if (puertasPorAbrir == 0):
+        result = [puertas, abiertas]
+        return result
+    
+    # Si el la puerta escogida no esta abierta ni es la que escogio el usuario ni
+    # la que esconde el premio.
+    elif not(ranInt in abiertas) and (ranInt != usuario) and (ranInt != premio):
+        # Abrimos la puerta.
+        abiertas.append(ranInt)
+        puertas[ranInt] = 1
+        puertasPorAbrir -= 1
+        return abrirPuertasAux(puertasPorAbrir, puertas,usuario, premio, abiertas) 
+    
+    # De lo contrario escoja otro valor.
+    else:
+        return abrirPuertasAux(puertasPorAbrir, puertas,usuario, premio, abiertas) 
         
-        # Verificamos que la posicion que escogimos no corresponda a
-        # la puerta del usuario ni del premio.
-        if puertas[ranInt] != 1 and puertas[ranInt] != 3 and puertas[ranInt] != -3:
-            
-            # Abrimos la puerta.
-            abiertas.append(ranInt)
-            puertas[ranInt] = 2
-            puertasPorAbrir -= 1
-    
-    result = cambioDePuerta(puertas, abiertas, cambiar)
-    return result
-
-def cambioDePuerta(puertas, abiertas, cambiar):
-    print("puertas sin cambio: ", puertas)
-    result = -1
-    
+      
+'''Se analiza si el usuario desea cambiar de puerta y se retorna el resultado del juego.
+param: puertas -> lista de puertas
+param: abiertas -> lista de puertas abiertas
+param: cambiar -> booleano que indica si se debe realizar un cambio
+param: premio -> numero de puerta donde se ubica el premio
+param: usuario -> puerta del usuario
+Retorna 1 o 0 indicando si gana o pierde.''' 
+def cambioDePuerta(puertas, abiertas, cambiar, premio, usuario):
     # Si no queremos cambiar
     if (cambiar == 0):
         # Si la puerta del usuario no corresponde al premio
-        if (3 in puertas):
+        if (usuario != premio):
             # Retornamos fracaso
             result = 0
         # Si la puerta del usuario corresponde al premio
-        elif (-3 in puertas):
+        else:
             # Retornamos exito
             result = 1
-        else: result = -1
             
     # Si el usuario desea cambiar       
     else:
         # Escogemos aleatoriamente una puerta cerrada 
-        print("puertas: ", puertas)
-        print("abiertas: ", abiertas)
-        ranPuerta = cicloRan(abiertas, len(puertas))
-        print("ranPuerta: ", ranPuerta)
+        ranPuerta = cicloRan(abiertas, len(puertas), usuario)
         
         # Si la puerta corresponde al premio
-        if (puertas[ranPuerta] == 1):
+        if (ranPuerta != premio):
             # Retornamos exito
-            result = 1
+            result = 0
         # Si la puerta no corresponde al premio
         else:
             # Retornamos fracaso
-            result = 0
+            result = 1
     
     return result
 
-
-def cicloRan(lista, rango):
+''' Funcion auxiliar para cambioPuerta. Busca una puerta cerrada para 
+realizar el cambio de puerta.
+param: lista -> lista de puertas abiertas
+param: rango -> total de puertas
+param: usuario -> puerta del usuario
+Retorna la puerta a la que se realiza el cambio. '''
+def cicloRan(lista, rango, usuario):
+    # Escogemos un valor random para la puerta que queremos cambiar.
     ranInt = random.randrange(rango)
-    if (ranInt in lista):
-        return cicloRan(lista, rango)
-    else:
+    
+    # Si la puerta no esta entre las abiertas ni corresponde a la del usuario.
+    if not(ranInt in lista) and (ranInt != usuario):
+        # Retorno la puerta que escogimos
         return ranInt
+    else:
+        # De lo contrario sigale dandonle.
+        return cicloRan(lista, rango, usuario)
+
+''' Verifica que la puerta escogida para esconder el premio no este abierta
+ni corresponda a la puerta del usuario.
+param: abiertas -> lista de puertas abiertas.
+param: puertasTotales -> el numero total de puertas.
+Retorna la puerta en la que se ubica el premio'''
+def verificarPremio(abiertas, puertasTotales):
+    premio = random.randrange(puertasTotales)
+    if not(premio in abiertas):
+        return premio
+    else:
+        return verificarPremio(abiertas, puertasTotales)
+
+'''Corre el ciclo de Monte Carlo para el caso de Monty Hall con n puertas.
+param: casos -> total de casos que se desean evaluar.
+param: puertasTotales-> el numero n de puertas totales que se desean.
+param: puertasPorAbrir -> el numero de puertas que se desean abrir.
+param: cambiar -> si se desea cambiar o no.'''
+def MonteCarlo(casos, puertasTotales, puertasPorAbrir, cambiar):
+    datos = []    
+    
+    while casos > 0:
+        # Construimos las puertas
+        puertas = definirPuertas(puertasTotales)
         
+        # Escondemos un premio
+        premio = random.randrange(puertasTotales)
         
-init()
+        # El jugador escoge una puerta
+        usuario = random.randrange(puertasTotales)
+        
+        # Abrimos una puerta
+        conjuntoPuertas = abrirPuertas(puertasPorAbrir, puertas, usuario, premio)
+        
+        #Calculamos el resultado
+        result = cambioDePuerta(conjuntoPuertas[0], conjuntoPuertas[1], cambiar, premio, usuario)
+                
+        #print(premio, usuario, conjuntoPuertas[0], cambiar, result)
+        datos.append(result)
+        casos -= 1
+                
+    print("cambiar", cambiar, Average(datos))
+   
+    
+''' Funcion que calcula el promedio de los numeros en una lista'''
+def Average(lst): 
+    return sum(lst) / len(lst) 
+
+
+numeroTotal = 3
+porAbrir = 1
+casos = 50000
+cambiar = 1
+MonteCarlo(casos, numeroTotal, porAbrir, cambiar)
+
